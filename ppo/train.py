7adcd7c0fb26d045bdf505127f97aeba54fd447c
David Zhang <davidwzhang@berkeley.edu>
2021-05-08 00:01:27 -0700

    PPO seems to be working

--- ppo/train.py
+++ ppo/train.py
@@ -68,39 +68,44 @@ class PPO():
 
         actor_losses = []
         critic_losses = []
+        entropy_losses = []
 
         for _ in range(self.n_epochs):
             for batch in self.rollout_buffer.get_dataloader(self.batch_size):
-                obs, actions, old_logprobs, old_values, _, _, _, _, returns, advantages = batch
+                obs, actions, old_logprobs, old_values, _, _, returns, advantages = batch
                 logprobs, entropy = self.policy.evaluate_actions(obs, actions)
                 values = self.policy.get_values(obs)
 
-                ratio = torch.exp(logprobs - old_logprobs)
-                actor_loss_1 = ratio*advantages
-                actor_loss_2 = torch.clamp(ratio, 1-self.clip_coef, 1+self.clip_coef)*advantages
-                actor_loss = -torch.mean(torch.min(actor_loss_1, actor_loss_2))
+                ratio = torch.exp(logprobs-old_logprobs)
+                actor_1 = ratio*advantages
+                actor_2 = torch.clamp(ratio, 1-self.clip_coef, 1+self.clip_coef)*advantages
+                actor_loss = -torch.mean(torch.min(actor_1, actor_2))
                 
-                critic_loss = self.critic_coef*F.mse_loss(values, returns)
-                
-                entropy_loss = -self.entropy_coef*torch.mean(entropy)
+                c_values = old_values + torch.clamp(values-old_values, -self.clip_coef, self.clip_coef)
+                critic_1 = torch.pow(c_values-returns, 2)
+                critic_2 = torch.pow(values-returns, 2)
+                critic_loss = 0.5*torch.mean(torch.max(critic_1, critic_2))
 
-                loss = actor_loss + critic_loss + entropy_loss
+                entropy_loss = -torch.mean(entropy)
+
+                loss = actor_loss + self.critic_coef*critic_loss + self.entropy_coef*entropy_loss
 
                 optimizer.zero_grad()
                 loss.backward()
-                # torch.nn.utils.clip_grad_norm_(self.policy.parameters(), 0.5)
+                torch.nn.utils.clip_grad_norm_(self.policy.parameters(), 0.5)
                 optimizer.step()
 
                 actor_losses.append(actor_loss.item())
                 critic_losses.append(critic_loss.item())
+                entropy_losses.append(entropy_loss.item())
 
-        return np.mean(actor_losses), np.mean(critic_losses)
+        return np.mean(actor_losses), np.mean(critic_losses), np.mean(entropy_losses)
 
     def learn(self):
         while self._steps < self.n_timesteps:
             self.collect_rollouts()
 
-            al, cl = self.train()
+            al, cl, el = self.train()
 
             self._steps += self.n_steps*self.n_envs
             print()
@@ -112,9 +117,7 @@ class PPO():
             print('Mean Length={}'.format(self.rollout_buffer.mean_episode_length))
             print('Actor Loss={}'.format(al))
             print('Critic Loss={}'.format(cl))
-
-            # if self.tensorboard:
-            #     self.writer.add_scalar('Reward', avg_reward, i)
+            print('Entropy Loss={}'.format(el))
 
             self._it += 1
             if self._it % 10 == 0:
@@ -133,7 +136,7 @@ class PPO():
                 self.eval_env.render()
                 with torch.no_grad():
                     actions, _ = self.policy.get_actions(obs)
-                    actions = actions.unsqueeze(dim=0).cpu().numpy()
+                    actions = actions.cpu().numpy()
                     # actions = actions.cpu().numpy()
                 obs, reward, done, info = self.eval_env.step(actions)
                 episode_rewards.append(reward)
@@ -158,24 +161,22 @@ class PPO():
                 values = values.cpu().numpy()
 
                 next_obs, rewards, dones, _ = self.env.step(actions)
-                next_values = self.policy.get_values(next_obs)
-                next_values = next_values.cpu().numpy()
 
             self.rollout_buffer.add_transition(
                 obs=self._obs, 
                 actions=actions, 
                 logprobs=logprobs, 
                 values=values, 
-                next_obs=next_obs, 
-                next_values=next_values,
                 rewards=rewards, 
                 dones=dones
             )
 
-            # if dones:
-            #     self._obs = self.env.reset()
-            # else:
             self._obs = next_obs
+
+        with torch.no_grad():
+            values = self.policy.get_values(self._obs)
+            values = values.cpu().numpy()
+            self.rollout_buffer.values[-1] = values
 
         self.rollout_buffer.compute_advantages(self.gamma, self.gae_lambda)
         self.rollout_buffer.compute_statistics()
@@ -228,17 +229,6 @@ class ImpalaBlock(nn.Module):
 class ActorCriticPolicy(nn.Module):
     def __init__(self):
         super(ActorCriticPolicy, self).__init__()
-        # self.features = nn.Sequential(
-        #     nn.Conv2d(in_channels=3, out_channels=32, kernel_size=8, stride=4, padding=2),
-        #     nn.ReLU(),
-        #     nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=1),
-        #     nn.ReLU(),
-        #     nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1),
-        #     nn.ReLU(),
-        #     nn.Flatten(),
-        #     nn.Linear(1024, 256),
-        #     nn.ReLU()
-        # )
         self.features = nn.Sequential(
             ImpalaBlock(in_channels=3, out_channels=16),
             ImpalaBlock(in_channels=16, out_channels=32),
@@ -248,16 +238,9 @@ class ActorCriticPolicy(nn.Module):
             nn.Linear(in_features=32*8*8, out_features=256),
             nn.ReLU()
         )
-        self.actor = nn.Sequential(
-            nn.Linear(256, 15)
-        )
-        self.critic = nn.Sequential(
-            nn.Linear(256, 1)
-        )
-
-        xavier_uniform_init(self.features)
-        orthogonal_init(self.actor, 0.01)
-        orthogonal_init(self.critic, 1.0)
+        self.features.apply(xavier_uniform_init)
+        self.actor = orthogonal_init(nn.Linear(256, 15), 0.01)
+        self.critic = orthogonal_init(nn.Linear(256, 1), 1.0)
 
         self.features = self.features.to(device)
         self.actor = self.actor.to(device)
@@ -272,7 +255,7 @@ class ActorCriticPolicy(nn.Module):
             obs = torch.from_numpy(obs).to(device, torch.float32)
         dis = self.forward(obs)
         actions = dis.sample()
-        return actions.squeeze(), dis.log_prob(actions)
+        return actions, dis.log_prob(actions)
 
     def evaluate_actions(self, obs, actions):
         if isinstance(obs, np.ndarray):
@@ -298,19 +281,15 @@ class RolloutBuffer():
         self.obs = np.zeros((self.n_steps, self.n_envs, *self.ob_dim), dtype=np.float32)
         self.actions = np.zeros((self.n_steps, self.n_envs))
         self.logprobs = np.zeros((self.n_steps, self.n_envs), dtype=np.float32)
-        self.values = np.zeros((self.n_steps, self.n_envs), dtype=np.float32)
-        self.next_obs = np.zeros((self.n_steps, self.n_envs, *self.ob_dim), dtype=np.float32)
-        self.next_values = np.zeros((self.n_steps, self.n_envs), dtype=np.float32)
+        self.values = np.zeros((self.n_steps+1, self.n_envs), dtype=np.float32)
         self.rewards = np.zeros((self.n_steps, self.n_envs), dtype=np.float32)
         self.dones = np.zeros((self.n_steps, self.n_envs), dtype=np.float32)
 
-    def add_transition(self, obs, actions, logprobs, values, next_obs, next_values, rewards, dones):
+    def add_transition(self, obs, actions, logprobs, values, rewards, dones):
         self.obs[self.index] = obs
         self.actions[self.index] = actions
         self.logprobs[self.index] = logprobs
         self.values[self.index] = values
-        self.next_obs[self.index] = next_obs
-        self.next_values[self.index] = next_values
         self.rewards[self.index] = rewards
         self.dones[self.index] = dones
         self.index += 1
@@ -319,23 +298,20 @@ class RolloutBuffer():
     def get_dataloader(self, batch_size):
         buffer_size = self.n_envs*self.n_steps
         i = np.random.choice(np.arange(buffer_size), buffer_size, replace=False)
-        # i = np.arange(buffer_size)
         j = 0
         while j < buffer_size:
-            s = np.index_exp[j:j+batch_size]
-            j += batch_size
+            k = i[j:j+batch_size]
             b = (
-                self.obs.reshape((buffer_size, *self.ob_dim))[i][s], 
-                self.actions.reshape(buffer_size)[i][s], 
-                self.logprobs.reshape(buffer_size)[i][s], 
-                self.values.reshape(buffer_size)[i][s], 
-                self.next_obs.reshape((buffer_size, *self.ob_dim))[i][s], 
-                self.next_values.reshape(buffer_size)[i][s], 
-                self.rewards.reshape(buffer_size)[i][s], 
-                self.dones.reshape(buffer_size)[i][s],
-                self.returns.reshape(buffer_size)[i][s],
-                self.advantages.reshape(buffer_size)[i][s]
+                self.obs.reshape((buffer_size, *self.ob_dim))[k], 
+                self.actions.reshape(buffer_size)[k], 
+                self.logprobs.reshape(buffer_size)[k], 
+                self.values[:-1].reshape(buffer_size)[k], 
+                self.rewards.reshape(buffer_size)[k], 
+                self.dones.reshape(buffer_size)[k],
+                self.returns.reshape(buffer_size)[k],
+                self.advantages.reshape(buffer_size)[k]
             )
+            j += batch_size
             yield tuple(map(lambda A: torch.from_numpy(A).to(device), b))
 
     def compute_advantages(self, gamma, gae_lambda):
@@ -344,12 +320,11 @@ class RolloutBuffer():
 
         A = 0
         for t in reversed(range(self.n_steps)):
-            delta = (self.rewards[t] + gamma*self.next_values[t]*(1-self.dones[t])) - self.values[t]
+            delta = (self.rewards[t] + gamma*self.values[t+1]*(1-self.dones[t])) - self.values[t]
             A = gamma*gae_lambda*A*(1-self.dones[t]) + delta
             self.advantages[t] = A
-        
-        self.returns = self.advantages + self.values
 
+        self.returns = self.advantages + self.values[:-1]
         self.advantages = (self.advantages-np.mean(self.advantages))/(np.std(self.advantages)+1e-6)
 
     def compute_statistics(self):
@@ -360,9 +335,9 @@ class RolloutBuffer():
             for s in range(self.n_steps):
                 episode_reward.append(self.rewards[s][e])
                 if self.dones[s][e]:
-                    episode_reward.clear()
-            episode_rewards.append(sum(episode_reward))
-            episode_lengths.append(len(episode_reward))
+                    episode_rewards.append(np.sum(episode_reward))
+                    episode_lengths.append(len(episode_reward))
+                    episode_reward = []
 
         self.min_episode_reward = np.min(episode_rewards)
         self.max_episode_reward = np.max(episode_rewards)
